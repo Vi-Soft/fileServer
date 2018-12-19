e317f84203ca08e4e165b91a22c8a1a68ae882e3
liga12 <Ligalauz12>
2018-12-19 11:53:38 +0200

    delete not needs

--- src/main/java/com/visoft/file/service/web/handler/RouterHandlerProvider.java
+++ src/main/java/com/visoft/file/service/web/handler/RouterHandlerProvider.java
@@ -10,26 +10,16 @@ import io.undertow.server.handlers.form.EagerFormParsingHandler;
 import io.undertow.server.handlers.form.FormParserFactory;
 import io.undertow.server.handlers.form.MultiPartParserDefinition;
 
-import static com.visoft.file.service.service.DI.DependencyInjectionService.FOLDER_SERVICE;
 import static io.undertow.util.Methods.GET;
 import static io.undertow.util.Methods.POST;
 
 public class RouterHandlerProvider implements HandlerProvider {
-
-    private String ROLE_ADMIN = "admin/";
-
-    private String USER = "users/";
-
-    private String FOLDERS = "folders/";
 
     private HttpHandler unzip = ReportService::unzip;
     private HttpHandler getRedImage = (exchange) -> ImageService.getImage(exchange, "r.png");
     private HttpHandler getGreyImage = (exchange) -> ImageService.getImage(exchange, "g.png");
     private HttpHandler login = AuthenticationService::login;
     private HttpHandler logout = AuthenticationService::logout;
-    private HttpHandler deleteFolder = FOLDER_SERVICE::deleteFolder;
-    private HttpHandler allFolders = FOLDER_SERVICE::findAllFolders;
-    private HttpHandler ss = AuthenticationService::sendFile;
 
     private EagerFormParsingHandler getEagerFormParsingHandler() {
         return new EagerFormParsingHandler(FormParserFactory.builder()
@@ -39,10 +29,6 @@ public class RouterHandlerProvider implements HandlerProvider {
     @Override
     public HttpHandler getHandler() {
         return Handlers.routing()
-                //all
-                .add(GET, "/l",
-                        getEagerFormParsingHandler()
-                                .setNext(ss))
                 .add(POST, "/logout",
                         getEagerFormParsingHandler()
                                 .setNext(logout))
