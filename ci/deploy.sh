f287e4ffb22adbe10a9d4be7d171bd320fca3032
Vlad Levin <vlad15june@gmail.com>
2022-02-03 16:44:39 +0200

    docker deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,5 @@
-cd ~/vi-soft/fileServer
+cd /home/visrvadmin/vi-soft/fileServer
 docker container stop file-server-app
 docker rmi file-server
 docker build -t file-server .
-docker run  --rm -d --network host --name file-server-app file-server
+docker run  --rm -d -v /home/visrvadmin/vi-soft/fileServer:/tmp  --network host --name file-server-app file-server
