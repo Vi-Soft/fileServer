8e4856fa18cf5189b649761e3d6bca0e7df858af
Vlad Levin <vlad15june@users.noreply.github.com>
2022-02-03 09:41:28 +0200

    Update deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,5 @@
 cd ~/vi-soft/fileServer
-docker container stop file-server
+docker container stop file-server-app
 docker rmi file-server
 docker build -t file-server .
 docker run  --rm -d --network host --name file-server-app file-server
