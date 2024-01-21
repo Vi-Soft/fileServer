a13401f903c1ee36d687d8b9b24d5974618a1f62
Vlad Levin <vlad15june@users.noreply.github.com>
2024-01-21 14:31:12 +0200

    Update ReportService.java

--- src/main/java/com/visoft/file/service/service/ReportService.java
+++ src/main/java/com/visoft/file/service/service/ReportService.java
@@ -339,8 +339,8 @@ public class ReportService {
 
             AuthenticatedUser authenticatedUser = SecurityHandler.authenticatedUser;
             User currentUser = authenticatedUser.getUser();
-            final Folder folder = FOLDER_SERVICE.findByFolder(shareDto.getFolder());
-
+            final Folder folder = FOLDER_SERVICE.findByFolder(File.separator + shareDto.getFolder());
+            
             if (folder != null) {
                 shareDto.getEmails().forEach(email -> {
                     User user = USER_SERVICE.findByLogin(email);
